592221c39eb851bb41a8267f4a9119672f524b6f
HunterMRocha <hmrocha2@dons.usfca.edu>
2022-09-13 14:51:49 -0700

    working on connecting rooms

--- main.py
+++ main.py
@@ -1,23 +1,101 @@
 import pygame
+import random
 from room import Room 
 
-pygame.init()
+global fonts
+rooms = []
+total_rooms = 7
+x = 150
+y = 150
 
 # CONSTANTS
-WIN_SIZE = (800, 800)
 BLUE = (0, 0, 255)
-
-screen = pygame.display.set_mode(WIN_SIZE)
-
-playing = True
-while playing:
-	for e in pygame.event.get():
-		if e.type == pygame.QUIT:
-			playing = False
+W, H = 50, 50
 
 
-	r = Room(100, 100, 50, 50)
-	r.draw(screen, BLUE)
+def init_screen_and_clock():
+	global screen, display, clock
+	pygame.init()
+	WIN_SIZE = (800, 800)
+	pygame.display.set_caption('D&D Map Gen')
+	screen = pygame.display.set_mode(WIN_SIZE)
+	clock = pygame.time.Clock()
+
+def create_fonts(font_sizes_list):
+	"Creates different fonts with one list"
+	global fonts
+	fonts = []
+	for size in font_sizes_list:
+		fonts.append(
+			pygame.font.SysFont("Arial", size))
+	return fonts
 
 
-pygame.quit()
+def render(fnt, item, col, pos): 
+	'Renders font from display_fps'
+	text = fnt.render(item, 0, pygame.Color(col))
+	screen.blit(text, pos)
+
+def display_fps():
+	'data that will be rendered and blitted in _display'
+	render(
+		fnt=fonts[0],
+		item=str(int(clock.get_fps())),
+		col='white',
+		pos=(0,0)
+	)
+
+def build():
+	'''
+	create and draw rooms
+	connect rooms after every two drawn rooms
+	'''
+	global x, y, W, H
+	roomGap = 40
+	for i in range(total_rooms):
+		rooms.append(Room(x, y, W, H, i+1))
+		rooms[i].draw(screen)
+		y = random.randint(50, 750)
+		x = random.randint(50, 750)
+		# make sure new room isn't overlapping old room pos  (still working on this)
+		while x < rooms[i].x + rooms[i].width + roomGap and x > rooms[i].x - rooms[i].width - roomGap:
+			x = random.randint(50, 750)
+		while y < rooms[i].y + rooms[i].height + roomGap and y > rooms[i].y - rooms[i].height - roomGap: 
+			y = random.randint(50, 750)
+
+		if i >= 1 and i % 2 == 1: 
+			# check horizontal connection
+			if rooms[i-1].x < rooms[i].x:
+				rooms[i-1].connectHor(screen, rooms[i])
+
+			# check vertical connection
+			elif rooms[i-1].y < rooms[i].y:
+				rooms[i-1].connectVert(screen, rooms[i])
+			
+
+def main():
+
+	init_screen_and_clock()
+	fonts = create_fonts([32, 16, 14, 8])
+
+	playing = True
+	while playing:
+		screen.fill((0, 0, 0))
+		display_fps()
+
+		for e in pygame.event.get():
+			if e.type == pygame.QUIT:
+				playing = False
+
+		# create and draw rooms
+		build()
+
+		clock.tick(60)
+
+		pygame.display.flip()
+
+
+	pygame.quit()
+
+if __name__ == '__main__': 
+	main()
--- room.py
+++ room.py
@@ -1,14 +1,50 @@
 import pygame
+pygame.init()
+
+DARK_BROWN = (99, 89, 56)
+rooms_pos = []
 
 class Room:
-	def __init__(self, x, y, w, h): 
+	def __init__(self, x, y, width, height, id):
 		self.x = x
 		self.y = y
-		self.w = w
-		self.h = h
-		self.rect = (x, y, w, h)
+		self.width = width
+		self.height = height
+		self.id = id
+		rooms_pos.append((self.x, self.y))
 
-	def draw(self, surf, col): 
-		pygame.draw.rect(surf, col, self.rect)
-
+	def draw(self, surf, color=DARK_BROWN):
+		pygame.draw.rect(surf, color, (self.x, self.y, self.width, self.height))
 	
+	def getBottomPos(self):
+		left = self.x + (self.width//2)
+		top = self.y + self.height
+		return (left, top)
+
+	def getTopPos(self):
+		return (self.x + (self.width//2), self.y)
+
+	def getRightPos(self):
+		x = self.x + self.width
+		return (x, self.y)
+
+	def getLeftPos(self):
+		return (self.x, self.y)
+		
+	def connectVert(self, surf, room2):
+		x1, y1 = self.getTopPos()
+		x2, y2 = room2.getBottomPos()
+		pygame.draw.line(surf,DARK_BROWN, (x1, y1), (x2, y2-5), 10)
+
+	def connectHor(self, surf, room2):
+		x1, y1 = self.getRightPos()
+		x2, y2 = room2.getLeftPos()
+		if y1 != y2:
+			cornerX = x2 + self.width/2
+			cornerY = y1 + self.height/2
+			pygame.draw.line(surf, DARK_BROWN, (x1, y1+self.width/2), (cornerX, cornerY), 10)
+			pygame.draw.line(surf, DARK_BROWN, (cornerX, cornerY-2), (x2+self.width/2, y2+self.width/2) , 10)
+		# pygame.draw.line(surf,DARK_BROWN, (x1, y1+self.width/2), (x2, y2+self.width/2), 10)
+
+	def connect(self, surf, room2):
+		pass
